fc0ff9f8db8899b9b78101f2e4cd16e2d2f8ec0f
Jazmin Cabasa Caña <canajaja0920@gmail.com>
2024-12-09 06:53:39 +0800

    Code_Challenge 3

---
+++ Code_Challenge3.py
@@ -0,0 +1,17 @@
+num1 = eval(input("Enter the first number:"))
+num2 = eval(input("Enter the second number:"))
+
+answer = num1 + num2
+print("\nThe sum of" , num1, "and", num2 , "is", answer)
+answer = num1 - num2
+print("The difference of" , num1, "and", num2 , "is", answer)
+answer = num1 * num2
+print("The product of" , num1, "and", num2, "is", answer)
+answer = num1 / num2
+print("The quotient of", num1, "and", num2, "is", answer)
+answer = num1 ** num2
+print(num1, "exponent by", num2, "is", answer)
+answer = num1 % num2
+print("The remainder of", num1, "and", num2, "is", answer)
+answer = num1 // num2
+print("The floor division of", num1, "and", num2, "is", answer)
