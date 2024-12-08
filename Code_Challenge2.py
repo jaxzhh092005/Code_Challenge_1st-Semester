46aecf35fce6ae6c3d258a2f91f1ae299e4b31cc
Jazmin Cabasa Caña <canajaja0920@gmail.com>
2024-12-09 06:52:32 +0800

    Code_Challenge 2

---
+++ Code_Challenge2.py
@@ -0,0 +1,3 @@
+name = input("What is your name?")
+
+print("\t\t\t\t\t\t\t\t*\n\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t*\t\t   "+"Hi ! "+ name+"\t\t\t*\n\t\t\t\t\t\t*\t*\t*\t*\t*\n\t\t\t\t\t\t\t*\t*\t*\n\t\t\t\t\t\t\t\t*")
